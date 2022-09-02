58ac7872b7398f7827af0ddf6b1c2f93b949dfe0
Mark Silva <mark.allen.silva@gmail.com>
2022-09-01 17:28:44 -0700

    enter key active

--- index.js
+++ index.js
@@ -10,27 +10,48 @@
 
 let items = []
 
+const enterKey = document.querySelector("#newItem-el")
 const newItemEl = document.getElementById("newItem-el")
 const addBtnEl = document.getElementById("addBtn-el")
 const todoListEl = document.getElementById("todoList-el")
 const removeBtnEl = document.getElementById("removeBtn-el")
+let item = document.getElementById("itemLabel")
 
 addBtnEl.addEventListener("click", function() {
-  items.push(newItemEl.value)
-  console.log(items)
-  render()
-  newItemEl.value = ""
+  if(newItemEl.value != ""){
+    items.push(newItemEl.value)
+    console.log(items)
+    render()
+    newItemEl.value = ""
+    console.log(newItemEl.value)
+  }
+})
+
+enterKey.addEventListener("keyup", (e) => {
+  if(newItemEl.value != ""){
+    if (e.keyCode === 13) {
+      items.push(newItemEl.value)
+      console.log(items)
+      render()
+      newItemEl.value = ""
+    }
+  }
 })
 
 function render() {
-  todoListEl.innerHTML += `
-    <ul id='todoUl'>
-      <li id='todoLi'>
-        <input type='checkbox'> 
-        ${items[items.length - 1]}
-      </li>
-    </ul>
-    <button id='removeBtn-el'>REMOVE ITEM</button>
-  `
+  let listItems = ""
+  for (let i = 0; i < items.length; i++) {
+    listItems += `
+        <li id="todoLi">
+          <input type="checkbox" id="item"> 
+          <label id="itemLabel"> ${items[i]} </label>
+        </li>
+    `
+  }
+  todoListEl.innerHTML = listItems
 }
 
+// when button is pushed, item disappears
+removeBtnEl.addEventListener("click", function() {
+
+})
